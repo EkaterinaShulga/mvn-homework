40dbcb32ca5cbebd47e58c5f9e0aefad0f44ffa1
EkaterinaShulga <102359023+EkaterinaShulga@users.noreply.github.com>
2026-06-30 11:20:49 +0300

    Задания по gradle

---
+++ mvn-test/src/test/java/sbp/school/SimpleTest.java
@@ -0,0 +1,14 @@
+package sbp.school;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        System.out.println("✅ ТЕСТ РАБОТАЕТ!");
+        assertTrue(true);
+    }
+}
